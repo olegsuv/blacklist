105ffdb0e79acf3524319671aa461c8de54cb656
ReenExe <ipourri@gmail.com>
2016-04-15 18:01:20 +0300

    test before code `add estate agent`

--- backend/tests/AppBundle/Controller/ApiEstateAgentControllerTest.php
+++ backend/tests/AppBundle/Controller/ApiEstateAgentControllerTest.php
@@ -1,14 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: yp
- * Date: 15.04.16
- * Time: 17:56
- */
 
 namespace Tests\AppBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class ApiEstateAgentControllerTest {
+class ApiEstateAgentControllerTest extends WebTestCase
+{
+    public function testAdd()
+    {
+        $client = static::createClient();
 
-} 
+        $client->request('POST', '/api/v1/estate', [
+            'phone' => '380630000000'
+        ]);
+
+        $this->assertEquals(201, $client->getResponse()->getStatusCode());
+    }
+}
