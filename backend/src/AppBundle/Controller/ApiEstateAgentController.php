115dd210b000ecec0aa4e0c6c2af621d1d823034
ReenExe <ipourri@gmail.com>
2016-04-15 18:30:47 +0300

    add `phone`

--- backend/src/AppBundle/Controller/ApiEstateAgentController.php
+++ backend/src/AppBundle/Controller/ApiEstateAgentController.php
@@ -2,14 +2,26 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\SEstateAgentPhone;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class ApiEstateAgentController extends Controller
 {
-    public function createAction()
+    public function createAction(Request $request)
     {
+        $phone = $request->request->get('phone');
+
+        $agentPhone = new SEstateAgentPhone();
+
+        $agentPhone
+            ->setPhone($phone)
+            ->setInserted(new \DateTime());
+
+        $this->get('doctrine')->getManager()->persist($agentPhone);
+        $this->get('doctrine')->getManager()->flush();
+
         return new JsonResponse([], JsonResponse::HTTP_CREATED);
     }
 }
