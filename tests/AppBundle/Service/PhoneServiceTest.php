48553296b8820c8846b06798f6ec683af39f49e2
ReenExe <ipourri@gmail.com>
2016-04-19 20:59:37 +0300

    test before code

--- tests/AppBundle/Service/PhoneServiceTest.php
+++ tests/AppBundle/Service/PhoneServiceTest.php
@@ -13,10 +13,33 @@ class PhoneServiceTest extends AbstractServiceTest
         $this->assertTrue($this->getService()->valid($phone));
     }
 
+    /**
+     * @dataProvider invalidDataProvider
+     * @param $phone
+     */
+    public function testInvalid($phone)
+    {
+        $this->assertFalse($this->getService()->valid($phone));
+    }
+
     public function validDataProvider()
     {
         return [
             ['380631112233'],
+            ['38063 111 22 33'],
+            ['38(063) 111-22-33'],
+            ['8(063) 111-22-33'],
+            ['(063) 111-22-33'],
+            ['063 111-22-33'],
+        ];
+    }
+
+    public function invalidDataProvider()
+    {
+        return [
+            [''],
+            ['111 22 33'],
+            ['09x xxxxxxx'],
         ];
     }
 
