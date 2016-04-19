96d8f82dcec8acce1c5278119b4997f2d70e0673
ReenExe <ipourri@gmail.com>
2016-04-19 21:07:17 +0300

    pass tests

--- src/AppBundle/Service/PhoneService.php
+++ src/AppBundle/Service/PhoneService.php
@@ -4,9 +4,19 @@ namespace AppBundle\Service;
 
 class PhoneService
 {
+    private $codes = ['039', '050', '063', '066', '067', '068', '073', '091', '092', '093', '094', '095', '096', '097', '098', '099'];
+
     public function valid($phone)
     {
-        return true;
+        $onlyNumber = preg_replace('/\D/', '', $phone);
+
+        if (strlen($onlyNumber) < 9) {
+            return false;
+        }
+
+        $code = substr($onlyNumber, -10, 3);
+
+        return in_array($code, $this->codes);
     }
 
     public function normalize($phone)
