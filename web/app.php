2e4c1602a81d61b13b58d1436bcc9b1b391c813b
ReenExe <ipourri@gmail.com>
2017-01-04 01:32:49 +0200

    try again

--- web/app.php
+++ web/app.php
@@ -26,9 +26,5 @@ $kernel->loadClassCache();
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
-$response->headers->add([
-    'Access-Control-Allow-Origin' => '*',
-    'Access-Control-Allow-Methods' => 'GET, POST'
-]);
 $response->send();
 $kernel->terminate($request, $response);
