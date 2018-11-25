881029997d76e952f4b814bbc4ed850a5b343b4e
Oleg Suvorov <oleg.suvorov@omnigon.com>
2018-11-25 17:02:27 +0200

    Clean

--- js/background.js
+++ js/background.js
@@ -2,25 +2,6 @@
  * Created by Oleg on 14.02.2017.
  */
 
-/* global chrome */
-/*const rule1 = {
-    conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {
-                hostEquals: 'www.olx.ua',
-                schemes: ['https']
-            }
-        })
-    ],
-    actions: [new chrome.declarativeContent.ShowPageAction()]
-};
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([rule1]);
-    });
-});*/
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    console.log('onUpdated', arguments);
+chrome.tabs.onUpdated.addListener(function(tabId) {
     chrome.tabs.sendMessage(tabId, 'url-update');
 });
